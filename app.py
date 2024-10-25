b8946adede975f8815172487e23e32d193d0b4db
Kushal190903 <144829202+Kushal190903@users.noreply.github.com>
2024-10-25 15:42:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,63 +3,111 @@ import re
 import string
 import numpy as np
 from keras.models import load_model
+import trafilatura
+from xgboost import XGBClassifier
+
+ 
+try:
+    model = load_model('news_classifier_neural_network_model.keras')
+    # model=XGBClassifier()
+    # model.load_model('news_classifier_xgb.json')
+except:
+    st.warning("Error loading the model. Please check the model file or retry by reloading the page")
+
+
+def wordopt(t):
+     t = t.lower()
+     t = re.sub('\[.*?\]', 'bracket', t)
+     t = re.sub("\\W"," ",t)
+     t = re.sub('https?://\S+|www\.\S+', 'hyperlink', t)
+     t = re.sub('<.*?>+', 'HTML', t)
+     t = re.sub('[%s]' % re.escape(string.punctuation), '', t)
+     t = re.sub('\n', '', t)
+     #t = re.sub('\w*\d\w*', '', t)
+     return t.strip()
+
+def classify(text):
+    '''classify the text'''
+    text=wordopt(text)
+    embedded_text = np.array(embedding.embed_query(text)).reshape(1, -1)
+    prediction = model.predict(embedded_text)
+    return prediction[0][0]  # Return the prediction score (0 for reliable, 1 for unreliable)
+
+def check_length_satisfaction(text,min_length):
+    '''check whether the length of a text(in words) is less than the specified number'''
+    length=0
+    i=0
+    while i<len(text):
+        if text[i]!=" ":
+            i+=1
+        else:
+            
+            while text[i]==" ":
+                i=i+1
+            length+=1
+        if length>=min_length:
+            break
+    if length<min_length:
+        st.warning('The prediction may not be accurate on short texts')
 
 try:
     from langchain_huggingface import HuggingFaceEmbeddings
 
     embedding = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
     
-    
-    try:
-        model = load_model('unreliable+news+detection+model.keras')
-    except:
-        st.warning("Error loading the model. Please check the model file or retry by reloading the page")
-    
-    
-    def wordopt(text):
-        text = text.lower()
-        text = re.sub('\[.*?\]', '', text)
-        text = re.sub(r"\W", " ", text)
-        text = re.sub(r'https?://\S+|www\.\S+', '', text)
-        text = re.sub(r'<.*?>+', '', text)
-        text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-        text = re.sub(r'\n', '', text)
-        text = re.sub(r'\w*\d\w*', '', text)
-        return text
-
-    
-    def classify(text):
-        cleaned_text = wordopt(text)
-        embedded_text = np.array(embedding.embed_query(cleaned_text)).reshape(1, -1)
-        prediction = model.predict(embedded_text)
-        return prediction[0][0]  
-
+   
   
     st.title("Unreliable News Detection with Confidence Score")
-
+    st.sidebar.title('About')
+    st.sidebar.write("This app classifies news as either reliable  or unreliable  and shows the confidence score.")
     
-    user_input = st.text_area("Enter news text to classify", "")
 
-    if st.button("Classify"):
-        if user_input:
-            try:
-              
-                result = classify(user_input)
+    # User input for text
+    input_option = st.radio("Select Input Type", ("URL", "Text"))
+    if input_option=='Text':
+
+        user_input = st.text_area("Enter news text to classify", "",height=50)
+
+    elif input_option=='URL':
+        user_input=st.text_area("Enter URL of the news article",height=10)
+
+    classify_button=st.button('classify')
+    if classify_button:
+        
+        try:
+            result=-1
+            # Get the classification result
+            if input_option=='Text':
+                if user_input:
+                    result = classify(user_input)
+                    
+                    check_length_satisfaction(user_input,100)
+                else:
+                    st.warning("Please enter some text for classification.")
+                    
+            else:
+                if not user_input:
+                    st.warning("please enter a URL")
+                else:
+                    downloaded = trafilatura.fetch_url(user_input)#userinput used here for the web url
+                    main_content = trafilatura.extract(downloaded) if downloaded else None 
+                    if main_content:
+                        st.write(main_content)
+                        result=classify(main_content)
+                        check_length_satisfaction(main_content,100)
+                    else:
+                        st.warning("Could not find any article or relevant text")
+                    
+            if result>=0:
                 confidence = result if result >= 0.5 else 1 - result
-
                 
                 if result < 0.5:
-                    st.success(f"The news is classified as Reliable  with a confidence of {round((1 - result)*100, 1)}%")
+                    st.success(f"The news is classified as Reliable  with a confidence of {round(confidence*100)}%")
                 else:
-                    st.error(f"The news is classified as Unreliable  with a confidence of {round(result * 100, 1)}%")
-            except Exception as e:
-                st.warning(f"An error occurred during classification: {str(e)}")
-            if len(user_input.split(" "))<100:
-                st.warning("The prediction may not be accurate on short texts")
-        else:
-            st.warning("Please enter some text for classification.")
-            
-    st.write("This app classifies news as either reliable (0) or unreliable (1) and shows the confidence score.")
+                    st.error(f"The news is classified as Unreliable  with a confidence of {round(confidence * 100)}%")
+        except Exception as e:
+            st.warning(f"An error occurred during classification: {str(e)}")
+        
 
 except Exception as e:
     st.warning(f"An unexpected error occurred: {str(e)}. Please reload the page.")
